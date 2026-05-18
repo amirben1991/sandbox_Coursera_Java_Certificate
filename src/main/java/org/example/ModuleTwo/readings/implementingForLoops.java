2a07b55a82e029cc4cc04f05947d9a356ec7232a
amirben1991 <amirben1991@gmail.com>
2026-05-18 23:49:05 +0200

    add implementingForLoops code

--- src/main/java/org/example/ModuleTwo/readings/implementingForLoops.java
+++ src/main/java/org/example/ModuleTwo/readings/implementingForLoops.java
@@ -1,4 +1,13 @@
 package org.example.ModuleTwo.readings;
 
 public class implementingForLoops {
+    public static void main(String[] args) {
+        // First example code
+        int scoopsSold;
+        System.out.println("Selling chocolate ice cream: 20 scoosp");
+        for (scoopsSold = 0; scoopsSold<20; scoopsSold++) {
+            System.out.println("Sold scoop No: " + (scoopsSold + 1));
+        }
+        System.out.println("All scoops sold: " + scoopsSold);
+    }
 }
